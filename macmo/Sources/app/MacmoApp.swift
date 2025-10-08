f0e3e74b0a7e81a071efd2f00057436b0e1f3833
신동규 <donggyu9410@gmail.com>
2025-10-08 19:16:49 +0900

    delete contentsize

--- macmo/Sources/app/MacmoApp.swift
+++ macmo/Sources/app/MacmoApp.swift
@@ -2,29 +2,26 @@ import SwiftUI
 
 @main
 struct MacmoApp: App {
-    
     @StateObject var navigationManager = NavigationManager()
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(navigationManager)
         }
-        
+
         WindowGroup("New Memo", id: "memo-detail") {
             MemoDetailView(model: MemoDetailViewModel(id: nil))
                 .environmentObject(navigationManager)
         }
-        .windowResizability(.contentSize)
-        .defaultSize(width: 500, height: 600)
+        .defaultSize(width: 600, height: 700)
 
         WindowGroup("Search Memos", id: "search-memo") {
             SearchMemoView(model: SearchMemoViewModel())
                 .environmentObject(navigationManager)
         }
-        .windowResizability(.contentSize)
         .defaultSize(width: 600, height: 700)
-        
+
         WindowGroup("Setting", id: "setting") {
             NavigationStack {
                 SettingView(model: .init())
