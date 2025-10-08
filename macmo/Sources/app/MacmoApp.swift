a9326679c923a8aab90cc75d48b931da11313470
신동규 <donggyu9410@gmail.com>
2025-10-08 19:51:31 +0900

    Add keyboard shortcut

--- macmo/Sources/app/MacmoApp.swift
+++ macmo/Sources/app/MacmoApp.swift
@@ -3,11 +3,30 @@ import SwiftUI
 @main
 struct MacmoApp: App {
     @StateObject var navigationManager = NavigationManager()
+    @Environment(\.openWindow) private var openWindow
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(navigationManager)
+        }
+        .commands {
+            CommandMenu("Memo") {
+                Button("New") {
+                    openWindow(id: "memo-detail")
+                }
+                .keyboardShortcut("n", modifiers: .command)
+
+                Button("Search") {
+                    openWindow(id: "search-memo")
+                }
+                .keyboardShortcut("f", modifiers: .command)
+
+                Button("Setting") {
+                    openWindow(id: "setting")
+                }
+                .keyboardShortcut("s", modifiers: .command)
+            }
         }
 
         WindowGroup("New Memo", id: "memo-detail") {
