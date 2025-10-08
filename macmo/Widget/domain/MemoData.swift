fb7c5aa36c80a6ac611fc369480aeb1abe6e2ff4
신동규 <donggyu9410@gmail.com>
2025-10-09 01:20:23 +0900

    make public

--- macmo/Widget/domain/MemoData.swift
+++ macmo/Widget/domain/MemoData.swift
@@ -1,10 +1,24 @@
 
 import Foundation
 
-struct MemoData: Identifiable {
-    let id: UUID
-    let title: String
-    let content: String
-    let createdAt: Date
-    let isCompleted: Bool
+public struct MemoData: Identifiable {
+    public let id: UUID
+    public let title: String
+    public let content: String
+    public let createdAt: Date
+    public let isCompleted: Bool
+
+    init(
+        id: UUID,
+        title: String,
+        content: String,
+        createdAt: Date,
+        isCompleted: Bool
+    ) {
+        self.id = id
+        self.title = title
+        self.content = content
+        self.createdAt = createdAt
+        self.isCompleted = isCompleted
+    }
 }
