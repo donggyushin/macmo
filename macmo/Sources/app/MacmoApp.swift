987ab2354593c80c27cb5910d2162db54d84632f
신동규 <donggyu9410@gmail.com>
2025-10-08 16:10:25 +0900

    chore

--- macmo/Sources/app/MacmoApp.swift
+++ macmo/Sources/app/MacmoApp.swift
@@ -13,12 +13,14 @@ struct MacmoApp: App {
         
         WindowGroup("New Memo", id: "memo-detail") {
             MemoDetailView(model: MemoDetailViewModel(id: nil))
+                .environmentObject(navigationManager)
         }
         .windowResizability(.contentSize)
         .defaultSize(width: 500, height: 600)
 
         WindowGroup("Search Memos", id: "search-memo") {
             SearchMemoView(model: SearchMemoViewModel())
+                .environmentObject(navigationManager)
         }
         .windowResizability(.contentSize)
         .defaultSize(width: 600, height: 700)
@@ -26,6 +28,7 @@ struct MacmoApp: App {
         WindowGroup("Setting", id: "setting") {
             NavigationStack {
                 SettingView(model: .init())
+                    .environmentObject(navigationManager)
             }
         }
         .defaultSize(width: 600, height: 700)
