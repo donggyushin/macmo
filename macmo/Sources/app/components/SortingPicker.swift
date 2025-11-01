549bcf217fadc0a2b21e50b44f857ff9aa84dbf5
donggyu <loopy@heyratel.com>
2025-11-01 16:24:46 +0900

    add sortingPicker

---
+++ macmo/Sources/app/components/SortingPicker.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct SortingPicker: View {
+    @Binding var sortBy: MemoSort
+
+    var body: some View {
+        HStack {
+            Picker("Sort by", selection: $sortBy) {
+                Text("Created").tag(MemoSort.createdAt)
+                Text("Updated").tag(MemoSort.updatedAt)
+                Text("Due").tag(MemoSort.due)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+        .padding(.horizontal)
+    }
+}
