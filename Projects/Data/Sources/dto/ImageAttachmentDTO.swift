0f0eee129498c969fc3a49c15ad3a8db8ed30b88
신동규 <donggyu9410@gmail.com>
2025-11-09 19:26:04 +0900

    add ImageAttachmentDTO

---
+++ Projects/Data/Sources/dto/ImageAttachmentDTO.swift
@@ -0,0 +1,32 @@
+import Foundation
+import MacmoDomain
+
+public struct ImageAttachmentDTO: Codable {
+    public let id: String
+    public let imageData: Data
+    public let order: Int
+    public let createdAt: Date
+
+    public init(
+        id: String,
+        imageData: Data,
+        order: Int,
+        createdAt: Date
+    ) {
+        self.id = id
+        self.imageData = imageData
+        self.order = order
+        self.createdAt = createdAt
+    }
+
+    public init(from domain: ImageAttachment) {
+        self.id = domain.id
+        self.imageData = domain.imageData
+        self.order = domain.order
+        self.createdAt = domain.createdAt
+    }
+
+    public var domain: ImageAttachment {
+        return .init(id: id, imageData: imageData, order: order, createdAt: createdAt)
+    }
+}
