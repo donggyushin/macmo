1b0aa63fcf7975334df687f94d6301b2a616a7bb
신동규 <donggyu9410@gmail.com>
2025-11-09 14:17:28 +0900

    inject memorepository to navigation manager for cache

--- macmo/Sources/app/navigation/NavigationManager.swift
+++ macmo/Sources/app/navigation/NavigationManager.swift
@@ -5,21 +5,28 @@
 //  Created by 신동규 on 10/3/25.
 //
 
-import Foundation
 import Combine
+import Factory
+import Foundation
 
 final class NavigationManager: ObservableObject {
-    
     @Published var paths: [Navigation] = []
-    
+    @Injected(\.memoRepository) private var memoRepository
+
+    init() {
+        bind()
+    }
+
     @MainActor
     func push(_ path: Navigation) {
         paths.append(path)
     }
-    
+
     @MainActor
     func pop() {
         guard !paths.isEmpty else { return }
         paths.removeLast()
     }
+
+    private func bind() {}
 }
