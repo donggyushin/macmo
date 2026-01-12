ea1711a0c887e5a7dbb990a0649684ef0bab8ade
donggyu <loopy@heyratel.com>
2026-01-12 13:58:02 +0900

    feat

--- Projects/App/Sources/list/day/SpecificDayMemoListViewModel.swift
+++ Projects/App/Sources/list/day/SpecificDayMemoListViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by ratel on 1/12/26.
 //
 
+import Combine
+import Factory
 import Foundation
+import MacmoDomain
+
+final class SpecificDayMemoListViewModel: ObservableObject {
+    @Injected(\.memoRepository) private var memoRepository
+    @Published var memos: [Memo] = []
+
+    let date: Date
+    init(date: Date) {
+        self.date = date
+    }
+
+    @MainActor func fetchMemos() throws {
+        memos = try memoRepository.findByDate(date)
+    }
+}
