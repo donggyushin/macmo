e1e25d2b7023b5bc594c855d606f098aae3bff34
DG <donggyu9410@gmail.com>
2026-01-16 14:38:03 +0900

    Calendar dot visible mode (#52)
    * feat
    
    * feat
    
    * feat
    
    * feat
    
    * feat
    
    ---------
    
    Co-authored-by: donggyu <loopy@heyratel.com>

---
+++ Projects/Domain/Sources/entity/CalendarDotVisibleMode.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarDotVisibleMode.swift
+//  MacmoDomain
+//
+//  Created by ratel on 1/16/26.
+//
+
+import Foundation
+
+public enum CalendarDotVisibleMode: Codable {
+    case all
+    case onlyNotCompleted
+}
