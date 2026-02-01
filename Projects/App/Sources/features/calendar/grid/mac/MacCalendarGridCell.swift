8e63e4bd4f438b871147432c2c66893ba3812329
신동규 <donggyu9410@gmail.com>
2026-02-01 13:33:21 +0900

    feat

--- Projects/App/Sources/features/calendar/grid/mac/MacCalendarGridCell.swift
+++ Projects/App/Sources/features/calendar/grid/mac/MacCalendarGridCell.swift
@@ -5,10 +5,42 @@
 //  Created by 신동규 on 2/1/26.
 //
 
+import MacmoDomain
 import SwiftUI
 
 struct MacCalendarGridCell: View {
+    let data: MacCalendarDayPresentation
     var body: some View {
         Text("MacCalendarGridCell")
     }
+}
+
+private struct MacCalendarGridCellPreview: View {
+    private let columns = Array(repeating: GridItem(.flexible(), spacing: 1), count: 7)
+
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 1) {
+            ForEach(1...28, id: \.self) { day in
+                MacCalendarGridCell(data: .init(year: 2026, month: 2, day: day, memos: day % 3 == 0 ? [
+                    .init(
+                        id: "\(day)",
+                        title: "title\(day)",
+                        contents: "contents",
+                        due: Date(),
+                        done: false,
+                        eventIdentifier: nil,
+                        createdAt: Date(),
+                        updatedAt: Date(),
+                        images: []
+                    )
+                ] : []))
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MacCalendarGridCellPreview()
+        .preferredColorScheme(.dark)
 }
