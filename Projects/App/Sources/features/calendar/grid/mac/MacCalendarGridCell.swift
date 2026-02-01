63bd580a0ee35205d7f7890f81272302bb0ee3cc
신동규 <donggyu9410@gmail.com>
2026-02-01 13:23:38 +0900

    chore

---
+++ Projects/App/Sources/features/calendar/grid/mac/MacCalendarGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MacCalendarGridCell.swift
+//  macmo
+//
+//  Created by 신동규 on 2/1/26.
+//
+
+import SwiftUI
+
+struct MacCalendarGridCell: View {
+    var body: some View {
+        Text("MacCalendarGridCell")
+    }
+}
