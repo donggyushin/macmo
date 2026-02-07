5a3cdc4914a90eedec9abad0e506fd0bed69cc79
신동규 <donggyu9410@gmail.com>
2026-02-07 17:23:01 +0900

    chore

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -13,8 +13,8 @@ let project = Project(
             "IPHONEOS_DEPLOYMENT_TARGET": "18.0",
             "DEVELOPMENT_TEAM": "YV58Q28W8Z",
             "CODE_SIGN_STYLE": "Automatic",
-            "MARKETING_VERSION": "1.17.0",
-            "CURRENT_PROJECT_VERSION": "25",
+            "MARKETING_VERSION": "1.18.0",
+            "CURRENT_PROJECT_VERSION": "26",
             "INFOPLIST_KEY_LSApplicationCategoryType": "public.app-category.productivity",
             "PRODUCT_NAME": "dgmemo",
             "INFOPLIST_KEY_CFBundleDisplayName": "dgmemo"
